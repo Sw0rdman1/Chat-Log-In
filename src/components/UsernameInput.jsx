89e8df70e0f1e6c09332604d4d09ade59f3cd0cc
Sw0rdman1 <vujasinovicb2019@gmail.com>
2022-10-04 15:09:51 +0200

    Updated icon for username

--- src/components/UsernameInput.jsx
+++ src/components/UsernameInput.jsx
@@ -5,53 +5,52 @@ import { AccountCircle } from "@mui/icons-material";
 import { Field } from "react-final-form";
 
 export default function InputWithIcon({ labelName, placeholder, fieldName }) {
-   return (
-      <div className="username-input">
-         <Box
-            sx={{
-               fontSize: "35px",
-               display: "flex",
-               alignItems: "flex-end",
-            }}
-         >
-            <Field name={fieldName}>
-               {({ input, meta }) => (
-                  <div style={{ fontSize: "25px" }}>
-                     <TextField
-                        InputProps={{ style: { fontSize: "25px" } }}
-                        InputLabelProps={{ style: { fontSize: "20px" } }}
-                        id="input-with-sx"
-                        label={labelName}
-                        variant="standard"
-                        type="text"
-                        {...input}
-                        color="secondary"
-                     />
-                     {meta.touched && meta.error && (
-                        <span
-                           style={{
-                              display: "block",
-                              color: "red",
-                              fontSize: "13px",
-                              paddingTop: "3px",
-                           }}
-                        >
-                           {meta.error}
-                        </span>
-                     )}
-                  </div>
-               )}
-            </Field>
-            <AccountCircle
-               sx={{
+  return (
+    <div className="username-input">
+      <Box
+        sx={{
+          fontSize: "35px",
+          display: "flex",
+          alignItems: "flex-end",
+        }}
+      >
+        <Field name={fieldName}>
+          {({ input, meta }) => (
+            <div style={{ fontSize: "25px" }}>
+              <TextField
+                InputProps={{ style: { fontSize: "25px" } }}
+                InputLabelProps={{ style: { fontSize: "20px" } }}
+                id="input-with-sx"
+                label={labelName}
+                variant="standard"
+                type="text"
+                {...input}
+                color="secondary"
+              />
+              <AccountCircle
+                sx={{
+                  paddingTop: "20px",
                   color: "action.active",
                   mr: 1,
                   my: 0.5,
                   fontSize: "30px",
-                  marginBottom: "10px",
-               }}
-            />
-         </Box>
-      </div>
-   );
+                }}
+              />
+              {meta.touched && meta.error && (
+                <p
+                  style={{
+                    color: "red",
+                    fontSize: "13px",
+                    marginTop: "2px",
+                  }}
+                >
+                  {meta.error}
+                </p>
+              )}
+            </div>
+          )}
+        </Field>
+      </Box>
+    </div>
+  );
 }
